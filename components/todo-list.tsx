34017a8715fcc85d9c704bc673055d07d7f0973b
cjl <jialiang.chen@sap.com>
2023-12-19 18:19:09 +0800

    dev: develop to-do list

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -7,62 +7,72 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
-
-const toDoListData = [
-    {
-        id: crypto.randomUUID(),
-        desc : "build a blog",
-        status : "build", //build/finish
-        sub_task : [
-            {
-                id: crypto.randomUUID(),
-                desc : "create a to-do list",
-                status : "build", //build/finish
-                sub_task : []
-            },
-            {
-                id: crypto.randomUUID(),
-                desc : "create a death-count-down clock",
-                status : "build", //build/finish
-                sub_task : []
-            },
-            {
-                id: crypto.randomUUID(),
-                desc : "create a gym plan",
-                status : "build", //build/finish
-                sub_task : []
-            },
-            {
-                id: crypto.randomUUID(),
-                desc : "create a live-share functionality",
-                status : "build", //build/finish
-                sub_task : []
-            },
-            {
-                id: crypto.randomUUID(),
-                desc : "create a gather-experience functionality",
-                status : "build", //build/finish
-                sub_task : []
-            }
-        ]
-    },
-    {
-        id: crypto.randomUUID(),
-        desc : "travel around world",
-        status : "build", //build/finish
-        sub_task : []
-    }
-]
+import { Progress } from "@/components/ui/progress"
 
 export function ToDoList() {
+    const toDoListData = [
+        {
+            id: crypto.randomUUID(),
+            desc : "build a blog",
+            status : "build", //build/finish
+            sub_task : [
+                {
+                    id: crypto.randomUUID(),
+                    desc : "create a to-do list",
+                    status : "build", //build/finish
+                    sub_task : []
+                },
+                {
+                    id: crypto.randomUUID(),
+                    desc : "create a death-count-down clock",
+                    status : "build", //build/finish
+                    sub_task : []
+                },
+                {
+                    id: crypto.randomUUID(),
+                    desc : "create a gym plan",
+                    status : "build", //build/finish
+                    sub_task : []
+                },
+                {
+                    id: crypto.randomUUID(),
+                    desc : "create a live-share functionality",
+                    status : "build", //build/finish
+                    sub_task : []
+                },
+                {
+                    id: crypto.randomUUID(),
+                    desc : "create a gather-experience functionality",
+                    status : "build", //build/finish
+                    sub_task : []
+                }
+            ]
+        },
+        {
+            id: crypto.randomUUID(),
+            desc : "travel around world",
+            status : "build", //build/finish
+            sub_task : []
+        }
+    ]
 
     return (
         <Accordion type="single" collapsible className="w-full">
             {toDoListData.map(task=>(
                 <AccordionItem key={task.id} value={task.id}>
-                    <AccordionTrigger>{task.desc}</AccordionTrigger>
+                    <AccordionTrigger>
+                        <div className="flex flex-row w-full items-center">
+                            <p className="w-1/6 flex justify-start">
+                                {task.desc}
+                            </p>
+                            <Progress value={33} />
+                            <p className={}>
+                                {33}%
+                            </p>
+                        </div>
+                    </AccordionTrigger>
                     {task.sub_task.map(subTask=>(
-                        <AccordionContent>
+                        <AccordionContent key={subTask.id}>
                             {subTask.desc}
                         </AccordionContent>
                     ))}
