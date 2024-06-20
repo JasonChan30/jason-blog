b730784b16486b73205b0282a8aeb2f3ce0d67f5
cjl <jialiang.chen@sap.com>
2024-06-20 17:59:57 +0800

    dev: dictionary

--- components/ui/dictionary/dictionary.tsx
+++ components/ui/dictionary/dictionary.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import { ChevronRight,ChevronDown } from "lucide-react"
+import {useState} from "react";
 
 const data = [
     {
@@ -23,13 +24,29 @@ const data = [
 
 
 export default function Dictionary() {
+    const [state, setState] = React.useState(true);
 
     return <div className="w-full h-48 bg-amber-200 overflow-y-auto">
         <div className="my-1.5 mx-2">
             <div className="flex flex-row justify-between items-center">
                 A
-                <ChevronRight className="h-4 w-4 shrink-0 transition-transform duration-200"/>
+                {state && (<ChevronRight className="h-4 w-4 shrink-0 transition-transform duration-200" onClick={() => {
+                    setState(false);
+                }}/>)}
+
+                {!state && ( <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" onClick={() => {
+                    setState(true);
+                }}/>)}
             </div>
+
+            {!state && (<ul className="border-l-2 pl-2">
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+                <li>A</li>
+            </ul>)}
         </div>
         <div className="my-1.5 mx-2">
             <div className="flex flex-row justify-between items-center">
