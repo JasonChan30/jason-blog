d706580c44792b6992cb3807cb104b2f355a6196
cjl <jialiang.chen@sap.com>
2024-06-21 17:59:33 +0800

    dev: dictionary

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ import Dictionary from '@/components/ui/dictionary/dictionary';
 
 export default function Home() {
 
-    return <div className="container my-2">
+    return <div className="m-4">
         <ResizablePanelGroup direction="horizontal">
-            <ResizablePanel defaultSize={20}>
+            <ResizablePanel defaultSize={15}>
                 <Dictionary/>
             </ResizablePanel>
             <ResizableHandle />
-            <ResizablePanel defaultSize={80}>
+            <ResizablePanel defaultSize={75}>
                 right
             </ResizablePanel>
             <ResizableHandle />
