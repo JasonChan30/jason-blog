d976ebe9b6178e7886c8448eb4523b496d52670e
cjl <jialiang.chen@sap.com>
2024-07-16 13:49:32 +0800

    dev: data table pagination

--- app/log/task/component/data-table-pagination.tsx
+++ app/log/task/component/data-table-pagination.tsx
@@ -8,6 +8,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import { ChevronRight,ChevronLeft,ChevronsRight,ChevronsLeft } from "lucide-react"
 
 interface DataTablePaginationProps<TData> {
     table: Table<TData>
@@ -15,21 +16,37 @@ interface DataTablePaginationProps<TData> {
 
 export function DataTablePagination<TData>({table}: DataTablePaginationProps<TData>) {
     return (
-        <div className="flex items-center justify-end space-x-2 py-4">
-            <p className="text-sm font-medium">Rows per page</p>
-            <Select onValueChange={value => {
-                table.setPageSize(Number(value))
-            }}>
-                <SelectTrigger className="w-[80px]">
-                    <SelectValue placeholder={table.getState().pagination.pageSize}/>
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="5">5</SelectItem>
-                    <SelectItem value="10">10</SelectItem>
-                    <SelectItem value="15">15</SelectItem>
-                </SelectContent>
-            </Select>
-            <p className="text-sm font-medium">Page  of </p>
+        <div className="flex items-center justify-end py-4 space-x-6 lg:space-x-8">
+            <div className="flex items-center">
+                <p className="text-sm font-medium">Rows per page</p>
+                <Select onValueChange={value => {
+                    table.setPageSize(Number(value))
+                }}>
+                    <SelectTrigger className="w-[80px]">
+                        <SelectValue placeholder={table.getState().pagination.pageSize}/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="5">5</SelectItem>
+                        <SelectItem value="10">10</SelectItem>
+                        <SelectItem value="15">15</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <p className="text-sm font-medium">Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}</p>
+            <div>
+                <Button variant="outline" size="icon"  onClick={() => table.firstPage()} disabled={!table.getCanPreviousPage()}>
+                    <ChevronsLeft className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" size="icon" onClick={()=>table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                    <ChevronLeft className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" size="icon" onClick={()=>table.nextPage()} disabled={!table.getCanNextPage()}>
+                    <ChevronRight className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" size="icon" onClick={() => table.lastPage()} disabled={!table.getCanNextPage()}>
+                    <ChevronsRight className="h-4 w-4" />
+                </Button>
+            </div>
         </div>
     )
 }
